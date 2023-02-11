220d2fec4c8dd62f05551b6189ac9feb654bcce5
cchet <herzog.thomas81@gmail.com>
2023-02-11 17:19:54 +0100

    Moved the frontend to frontend directory. Finished AWS CDK infra definition

--- aws/src/main/java/dev/ihet/aws/amplify/AwsApp.java
+++ aws/src/main/java/dev/ihet/aws/amplify/AwsApp.java
@@ -7,29 +7,14 @@ import software.amazon.awscdk.StackProps;
 public class AwsApp {
         public static void main(final String[] args) {
                 App app = new App();
+                var config = Configuration.load();
 
-                new AwsStack(app, "AwsStack", StackProps.builder()
-                                // If you don't specify 'env', this stack will be environment-agnostic.
-                                // Account/Region-dependent features and context lookups will not work,
-                                // but a single synthesized template can be deployed anywhere.
-
-                                // Uncomment the next block to specialize this stack for the AWS Account
-                                // and Region that are implied by the current CLI configuration.
-                                /*
-                                 * .env(Environment.builder()
-                                 * .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                                 * .region(System.getenv("CDK_DEFAULT_REGION"))
-                                 * .build())
-                                 */
+                new AwsStack(app, config.name + "-aws-stack", StackProps.builder()
                                 .env(Environment.builder()
-                                                .account("685519830705")
-                                                .region("eu-central-1")
+                                                .account(config.account)
+                                                .region(config.region)
                                                 .build())
-
-                                // For more information, see
-                                // https://docs.aws.amazon.com/cdk/latest/guide/environments.html
                                 .build());
-
                 app.synth();
         }
 }
--- aws/src/main/java/dev/ihet/aws/amplify/AwsStack.java
+++ aws/src/main/java/dev/ihet/aws/amplify/AwsStack.java
@@ -3,46 +3,78 @@ package dev.ihet.aws.amplify;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 
 import software.amazon.awscdk.CfnTag;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.amplify.CfnApp;
 import software.amazon.awscdk.services.amplify.CfnApp.AutoBranchCreationConfigProperty;
+import software.amazon.awscdk.services.amplify.CfnBranch;
+import software.amazon.awscdk.services.amplify.CfnDomain;
 import software.constructs.Construct;
 
 public class AwsStack extends Stack {
 
-    static final String NAME = "ihet.dev";
+        private final Configuration config;
 
-    public AwsStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
+        public AwsStack(final Construct scope, final String id) {
+                this(scope, id, null);
+        }
 
-    public AwsStack(final Construct scope, final String id, final StackProps props) {
-        super(scope, id, props);
-
-        CfnApp.Builder.create(this, buildName("app"))
-                .name(buildName("-app"))
-                .accessToken("ACCESS_TOKEN") // From arguments!!!!
-                .description("The www.ihet.dev application")
-                .platform("WEB")
-                .repository("https://github.com/cchet/ihet.dev")
-                .autoBranchCreationConfig(AutoBranchCreationConfigProperty.builder()
-                        .autoBranchCreationPatterns(List.of("deploy"))
-                        .enableAutoBranchCreation(true)
-                        .enablePerformanceMode(true)
-                        .enableAutoBuild(true)
-                        .build())
-                .tags(List.of(
+        public AwsStack(final Construct scope, final String id, final StackProps props) {
+                super(scope, id, props);
+                this.config = Configuration.load();
+                // standard tags
+                var tags = List.of(
                         CfnTag.builder().key("timestamp")
-                                .value(DateTimeFormatter.ISO_DATE_TIME.format(LocalDateTime.now())).build(),
+                                        .value(DateTimeFormatter.ISO_DATE_TIME
+                                                        .format(LocalDateTime.now()))
+                                        .build(),
                         CfnTag.builder().key("owner").value(getAccount()).build(),
-                        CfnTag.builder().key("name").value(NAME).build()))
-                .build();
-    }
+                        CfnTag.builder().key("name").value(config.name).build());
 
-    private String buildName(String suffix) {
-        return NAME + "-" + suffix;
-    }
+                // Create the amplify application
+                var app = CfnApp.Builder.create(this, buildResourceId("app"))
+                                .name(config.name)
+                                .accessToken(config.accessToken)
+                                .platform("WEB")
+                                .repository(config.repository)
+                                .autoBranchCreationConfig(AutoBranchCreationConfigProperty.builder()
+                                                .autoBranchCreationPatterns(List.of("deploy"))
+                                                .enableAutoBranchCreation(true)
+                                                .enablePerformanceMode(true)
+                                                .enableAutoBuild(true)
+                                                .build())
+                                .enableBranchAutoDeletion(true)
+                                .tags(tags)
+                                .build();
+
+                // Connect the branch which gets deployed automatically on a change
+                var branch = CfnBranch.Builder.create(this, buildResourceId("branch-deploy"))
+                                .appId(app.getAttrAppId())
+                                .branchName("deploy")
+                                .enableAutoBuild(true)
+                                .enablePerformanceMode(true)
+                                .framework("javascript")
+                                .tags(tags)
+                                .build();
+                branch.addDependency(app);
+
+                // Create the domain settings
+                var domain = CfnDomain.Builder.create(this, buildResourceId("domain"))
+                                .appId(app.getAttrAppId())
+                                .domainName(config.domain)
+                                .enableAutoSubDomain(true)
+                                .subDomainSettings(List.of(
+                                                Map.of(
+                                                                "branchName", "deploy",
+                                                                "prefix", "www")))
+                                .build();
+                domain.addDependency(branch);
+        }
+
+        private String buildResourceId(String suffix) {
+                return config.name + "-" + suffix;
+        }
 }
